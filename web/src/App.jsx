0ebec40c98efe24b49db2fc5eea662c99ee78703
Bernardo-Mattos <bernardomtt2@gmail.com>
2023-10-05 11:11:31 -0300

    ultimas alterações

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,12 +1,31 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/header";
+import Main from "./components/main";
+import MainP from "./components/principais";
+import Entradas from "./components/Entradas";
+import Vinhos from "./components/Vinhos";
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-     
+    <div className="container">
+      <Header/>
+      <main>
+        <Main />
+        <div className="pratosPrincipais">
+        <h1>Pratos Principais</h1>
+        <MainP />
+      </div>
+      <div className="entradas">
+        <h1>Entradas</h1>
+        <Entradas />
+      </div>
+      <div className="vinhos">
+        <h1>Vinhos</h1>
+        <Vinhos/>
+      </div>
+      </main>
+      
     </div>
   );
 }
-
-export default App;
