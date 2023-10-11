3fbf34c8342dfdb224a0fcb4c196202b39ca424a
Bernardo Augusto <113487013+Bernardo-Mattos@users.noreply.github.com>
2023-10-10 22:57:29 -0300

    ViaLagoa - v1.2 - Refatorando o projeto

--- web/src/components/Vinhos/index.jsx
+++ web/src/components/Vinhos/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../principais/components/card'
+import Card from '../card'
 import './styles.css'
 
 export default function Vinhos() {
@@ -10,21 +10,21 @@ export default function Vinhos() {
           "https://www.casadabebida.com.br/img/products/3-vinhos-pera-manca-tinto-750-ml-caixa-de-madeira_1_600.jpg"
         }
         preco="R$629,00 Vinho Branco Pera Manca"
-        desc="O vinho branco Pêra Manca é uma prestigiosa criação do Alentejo, Portugal, conhecida por sua elegância e qualidade. Possui aromas cítricos, notas florais e sabor suave, sendo versátil para acompanhar diversos pratos, como frutos do mar e aves."
+        desc="O vinho branco Pêra Manca é uma prestigiosa criação do Alentejo, Portugal, conhecida por sua elegância e qualidade"
       />
       <Card
         image={
           "https://images.tcdn.com.br/img/img_prod/722924/vinho_catena_malbec_750ml_221_1_fa1ff32aa256831db3fcfc11ac57e650.jpg"
         }
         preco="R$881,62,00 Vinho tinto Catena Zapata"
-        desc="O Catena Zapata é um prestigiado vinho argentino, famoso pelo Malbec de alta qualidade produzido pela Bodega Catena Zapata. Apresenta cor roxa intensa, aromas de frutas vermelhas e escuras, taninos suaves e um final longo."
+        desc="O Catena Zapata é um prestigiado vinho argentino, famoso pelo Malbec de alta qualidade produzido pela Bodega Catena Zapata"
       />
       <Card
         image={
           "https://m.media-amazon.com/images/I/71vKp9cRw7L.jpg"
         }
         preco="R$699,00 Espumante Champagne Moet"
-        desc="O Champagne Moët é um renomado espumante francês da região de Champagne, produzido pela Maison Moët & Chandon, conhecido por sua qualidade, elegância e sabor refinado, sendo uma escolha icônica para celebrações globais."
+        desc="O Champagne Moët é um renomado espumante francês da região de Champagne, produzido pela Maison Moët & Chandon."
       />
     </div>
   )
--- web/src/components/principais/index.jsx
+++ web/src/components/principais/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./components/card";
+import Card from "../card";
 import './styles.css'
 
 export default function MainP() {
@@ -10,14 +10,14 @@ export default function MainP() {
           "https://s2.glbimg.com/KhR2MlMX_KJFDe6QWNzBSiJZi2Q=/620x455/e.glbimg.com/og/ed/f/original/2018/08/10/ostras.jpg"
         }
         preco="R$40,00 Ostra gratinada"
-        desc="Ostras frescas cobertas com queijo derretido, ervas e migalhas de pão crocante, assadas até dourar. Uma experiência sofisticada que combina o sabor marinho das ostras com a suavidade do queijo e a crocância das migalhas de pão. Perfeito para os amantes de frutos do mar."
+        desc="Ostras frescas cobertas com queijo derretido, ervas e migalhas de pão crocante, assadas até dourar."
       />
       <Card
         image={
           "https://img.freepik.com/fotos-premium/file-mignon-com-molho-de-vinho-tinto_538646-8000.jpg"
         }
         preco="R$50,00 File Mingon"
-        desc=" Um corte nobre de carne bovina, preparado com perfeição, servido com um molho sofisticado à base de vinho tinto, acompanhado de legumes frescos e purê de batata trufado"
+        desc=" Um corte nobre de carne bovina, preparado com perfeição."
       />
       <Card
         image={
