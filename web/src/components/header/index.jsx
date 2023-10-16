0a1a42dd7dc0b6eaedb4d7aa4e924ac3ea9076d2
Bernardo Augusto <113487013+Bernardo-Mattos@users.noreply.github.com>
2023-10-16 10:29:07 -0300

    Update index.jsx

--- web/src/components/header/index.jsx
+++ web/src/components/header/index.jsx
@@ -12,7 +12,7 @@ export default function Header() {
         <a href="#">História</a>
         <a href="#">Menu</a>
         <a href="#">Reservas</a>
-        <a href="#">Contate-nos</a>
+        <a href="https://www.google.com/search?q=restaurante++floripa+&sca_esv=573777804&rlz=1C1GCEA_enBR1043BR1043&sxsrf=AM9HkKndsLGRf-VETIrWRYtfqVlDoeGWhA%3A1697462920254&ei=iDotZeiWD8jY5OUP86mCuAM&ved=0ahUKEwjo-oWT1vqBAxVILLkGHfOUADcQ4dUDCBA&uact=5&oq=restaurante++floripa+&gs_lp=Egxnd3Mtd2l6LXNlcnAiFXJlc3RhdXJhbnRlICBmbG9yaXBhIDIIEAAYywEYgAQyCBAAGMsBGIAEMggQABjLARiABDIIEAAYywEYgAQyCBAAGMsBGIAEMggQABjLARiABDIIEAAYywEYgAQyBhAAGBYYHjIGEAAYFhgeMgYQABgWGB5I1VRQ1gZY5URwBngBkAEAmAHIAaABxxmqAQYwLjIwLjG4AQPIAQD4AQGoAgrCAgoQABhHGNYEGLADwgIKEAAYigUYsAMYQ8ICBxAjGOoCGCfCAgcQIxiKBRgnwgINEC4YigUYxwEY0QMYQ8ICBRAuGIAEwgIFEAAYgATCAgcQABiKBRhDwgIEECMYJ8ICChAAGIoFGMkDGEPCAggQABiKBRiSA8ICCBAuGMsBGIAEwgIOEC4YywEYgAQYxwEYrwHCAgcQIxixAhgn4gMEGAAgQYgGAZAGCQ&sclient=gws-wiz-serp#rlimm=8843604423608663753">Contate-nos</a>
       </div>
     </div>
   );
